a21e99612e4197d3ec01106ce40997c0bfea2b96
g.rrustemi <g.rrustemi@reply.de>
2023-07-19 15:46:13 +0200

    Added more test cases (all tests are passing)

--- src/test/java/com/github/arena/challenges/weakmdparser/MarkdownParserTest.java
+++ src/test/java/com/github/arena/challenges/weakmdparser/MarkdownParserTest.java
@@ -118,4 +118,80 @@ class MarkdownParserTest {
 
         assertEquals(expected, markdownParser.parse(input));
     }
+
+    @Test
+    public void withH3HeaderLevel() {
+        String input = "### This will be an h3";
+        String expected = "<h3>This will be an h3</h3>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withH4HeaderLevel() {
+        String input = "#### This will be an h4";
+        String expected = "<h4>This will be an h4</h4>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withH5HeaderLevel() {
+        String input = "##### This will be an h5";
+        String expected = "<h5>This will be an h5</h5>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withParagraphAndBold() {
+        String input = "This will be an paragraph with __bold__ text";
+        String expected = "<p>This will be an paragraph with <strong>bold</strong> text</p>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withUnorderedListAndBoldText() {
+        String input = "* Not bold item 1\n* __Bold__ item 1";
+        String expected = "<ul><li>Not bold item 1</li><li><strong>Bold</strong> item 1</li></ul>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withUnorderedListAndItalicText() {
+        String input = "* Not italic item 1\n* _Italic_ item 1";
+        String expected = "<ul><li>Not italic item 1</li><li><em>Italic</em> item 1</li></ul>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void withUnorderedListAndBoldAndItalicText() {
+        String input = "* This is _italic_ and __bold__\n* This will not be italic and not bold";
+        String expected = "<ul><li>This is <em>italic</em> and <strong>bold</strong></li><li>This will not be italic and not bold</li></ul>";
+        
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void multipleGrowingHeaders() {
+        String input = "# This is h1\n## This is h2\n### This is h3\n#### This is h4";
+        String expected = "<h1>This is h1</h1><h2>This is h2</h2><h3>This is h3</h3><h4>This is h4</h4>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
+
+    @Test
+    public void everythingMixed() {
+        String input = "This is a title\n# This is a h1\n## This is a h2\n### This is a h3\n#### This is a h4\n##### This" +
+                " is a h5\n###### This is a h6\n This is a __bold__ text and an _italic_ text and here follows an\n" +
+                "* unordered list\n* with some values";
+        String expected = "<p>This is a title</p><h1>This is a h1</h1><h2>This is a h2</h2><h3>This is a h3</h3><h4>This is a h4" +
+                "</h4><h5>This is a h5</h5><h6>This is a h6</h6><p> This is a <strong>bold</strong> text and an <em>italic</em> text" +
+                " and here follows an</p><ul><li>unordered list</li><li>with some values</li></ul>";
+
+        assertEquals(expected, markdownParser.parse(input));
+    }
 }
